43e44d090e34c030cc64d8ed21586893da16a397
Sean Hellum <seanhellum45@gmail.com>
2019-08-23 08:48:21 -0500

    Update chessBoard.py

--- chessBoard.py
+++ chessBoard.py
@@ -2,17 +2,22 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
-def getImage(path):
-    return OffsetImage(plt.imread(path))
+def board():
+  def getImage(path):
+      return OffsetImage(plt.imread(path))
 
-paths = ['Pawn.png']
+  paths = ['Pawn.png','Pawn.png','Pawn.png','Pawn.png','Pawn.png','Pawn.png','Pawn.png','Pawn.png',
+          'Bishop.png','Bishop.png','blackBishop.png','blackBishop.png']
 
-x = [170]
-y = [960]
-fig, ax = plt.subplots()
-ax.scatter(x, y)
-img = mpimg.imread('chessBoard.png')
-imgplot = plt.imshow(img)
-for x0, y0, path in zip(x, y,paths):
-    ab = AnnotationBbox(getImage(path), (x0, y0), frameon=False)
-    ax.add_artist(ab)
+  x = [170,320,440,590,710,830,980,1100,440,830,440,830]
+  y = [960,960,960,960,960,960,960,960,1100,1100,180,180]
+  fig, ax = plt.subplots()
+  ax.scatter(x, y)
+  # plt.xticks([])
+  # plt.yticks([])
+  img = mpimg.imread('chessBoard.png')
+  imgplot = plt.imshow(img)
+  for x0, y0, path in zip(x, y,paths):
+      ab = AnnotationBbox(getImage(path), (x0, y0), frameon=False)
+      ax.add_artist(ab)
+board()
