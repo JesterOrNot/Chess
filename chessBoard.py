465287993a06c5ccf31f69f65455c571a0d4a301
Sean Hellum <seanhellum45@gmail.com>
2019-08-22 21:32:34 -0500

    Update chessBoard.py

--- chessBoard.py
+++ chessBoard.py
@@ -1,7 +1,18 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
-def board():
-    img = mpimg.imread('chessBoard.png')
-    imgplot = plt.imshow(img)
-board()
+def getImage(path):
+    return OffsetImage(plt.imread(path))
+
+paths = ['Pawn.png']
+
+x = [170]
+y = [960]
+fig, ax = plt.subplots()
+ax.scatter(x, y)
+img = mpimg.imread('chessBoard.png')
+imgplot = plt.imshow(img)
+for x0, y0, path in zip(x, y,paths):
+    ab = AnnotationBbox(getImage(path), (x0, y0), frameon=False)
+    ax.add_artist(ab)
