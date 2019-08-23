6166392a7204ae3d9e5f8ca31c2f7d408705aa43
Blurryface05 <seahel1@nilesk12.org>
2019-08-23 18:03:12 +0000

    new

--- chessBoard.py
+++ chessBoard.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+import __init__
+pieces = __init__.pieces()
 
 def board():
   def getImage(path):
       return OffsetImage(plt.imread(path))
 
-  paths = ['Pawn.png','Pawn.png','Pawn.png','Pawn.png','Pawn.png','Pawn.png','Pawn.png','Pawn.png',
-          'Bishop.png','Bishop.png','blackBishop.png','blackBishop.png']
+  paths = [pieces.whitePawn1.paths ,pieces.whitePawn2.paths,pieces.whitePawn3.paths,pieces.whitePawn4.paths,pieces.whitePawn5.paths,pieces.whitePawn6.paths,pieces.whitePawn7.paths,pieces.whitePawn1.paths,pieces.whiteBishop1.paths]
 
   x = [170,320,440,590,710,830,980,1100,440,830,440,830]
   y = [960,960,960,960,960,960,960,960,1100,1100,180,180]
