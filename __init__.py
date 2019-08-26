791b6f9c41690aa392ffa8d1457bcb1519d532c0
Sean Hellum <seanhellum45@gmail.com>
2019-08-26 17:49:10 -0500

    modified:   __init__.py

--- __init__.py
+++ __init__.py
@@ -1,193 +1,224 @@
 class pieces:
     class whitePawn1:
-        paths="Sprites/whitePawn.png"
-        Position= {
-            'x':170,
-            'y':960,
+        paths = "Sprites/whitePawn.png"
+        Position = {
+            'x': 170,
+            'y': 960,
         }
+
     class whitePawn2:
-        paths="Sprites/whitePawn.png"
+        paths = "Sprites/whitePawn.png"
         Position = {
-            'x':320,
-            'y':960,
+            'x': 320,
+            'y': 960,
         }
+
     class whitePawn3:
-        paths="Sprites/whitePawn.png"
+        paths = "Sprites/whitePawn.png"
         Position = {
-            'x':440,
-            'y':960,
+            'x': 440,
+            'y': 960,
         }
+
     class whitePawn4:
-        paths="Sprites/whitePawn.png"
+        paths = "Sprites/whitePawn.png"
         Position = {
-            'x':590,
-            'y':960,
+            'x': 590,
+            'y': 960,
         }
+
     class whitePawn5:
-        paths="Sprites/whitePawn.png"
-        Position= {
-            'x':710,
-            'y':960,
+        paths = "Sprites/whitePawn.png"
+        Position = {
+            'x': 710,
+            'y': 960,
         }
+
     class whitePawn6:
-        paths="Sprites/whitePawn.png"
+        paths = "Sprites/whitePawn.png"
         Position = {
-            'x':830,
-            'y':960,
+            'x': 830,
+            'y': 960,
         }
+
     class whitePawn7:
-        paths="Sprites/whitePawn.png"
+        paths = "Sprites/whitePawn.png"
         Position = {
-            'x':980,
-            'y':960,
+            'x': 980,
+            'y': 960,
         }
+
     class whitePawn8:
-        paths="Sprites/whitePawn.png"
-        Position ={
-            'x':1100,
-            'y':960,
+        paths = "Sprites/whitePawn.png"
+        Position = {
+            'x': 1100,
+            'y': 960,
         }
+
     class whiteBishop1:
-        paths="Sprites/whiteBishop.png"
-        Position={
-            'x':440,
-            'y':1100,
+        paths = "Sprites/whiteBishop.png"
+        Position = {
+            'x': 440,
+            'y': 1100,
         }
+
     class whiteBishop2:
-        paths="Sprites/whiteBishop.png"
-        Position={
-            'x':830,
-            'y':1100,
+        paths = "Sprites/whiteBishop.png"
+        Position = {
+            'x': 830,
+            'y': 1100,
         }
+
     class blackBishop1:
-        paths='Sprites/blackBishop.png'
-        Position={
-            'x':440,
-            'y':180,
+        paths = 'Sprites/blackBishop.png'
+        Position = {
+            'x': 440,
+            'y': 180,
         }
+
     class blackBishop2:
-        paths="Sprites/blackBishop.png"
-        Position={
-            'x':830,
-            'y':180,
+        paths = "Sprites/blackBishop.png"
+        Position = {
+            'x': 830,
+            'y': 180,
         }
+
     class whiteRook1:
-        paths='Sprites/whiteRook.png'
-        Position={
-            'x':185,
-            'y':1088,
+        paths = 'Sprites/whiteRook.png'
+        Position = {
+            'x': 185,
+            'y': 1088,
         }
+
     class whiteRook2:
-        paths="Sprites/whiteRook.png"
-        Position={
-            'x':1103,
-            'y':1088,
+        paths = "Sprites/whiteRook.png"
+        Position = {
+            'x': 1103,
+            'y': 1088,
         }
+
     class whiteKnight1:
-        paths='Sprites/whiteKnight.png'
-        Position={
-            'x':966,
-            'y':1088,
+        paths = 'Sprites/whiteKnight.png'
+        Position = {
+            'x': 966,
+            'y': 1088,
         }
+
     class whiteKnight2:
-        paths="Sprites/whiteKnight.png"
-        Position={
-            'x':327,
-            'y':1088,
+        paths = "Sprites/whiteKnight.png"
+        Position = {
+            'x': 327,
+            'y': 1088,
         }
+
     class whiteQueen:
-        paths="Sprites/whiteQueen.png"
-        Position={
-            'x':585,
-            'y':1100,
+        paths = "Sprites/whiteQueen.png"
+        Position = {
+            'x': 585,
+            'y': 1100,
         }
+
     class whiteKing:
-        paths="Sprites/whiteKing.png"
-        Position={
-            'x':704,
-            'y':1100,
+        paths = "Sprites/whiteKing.png"
+        Position = {
+            'x': 704,
+            'y': 1100,
         }
+
     class blackPawn1:
-        paths='Sprites/blackPawn.png'
-        Position={
-            'x':165,
-            'y':295,
+        paths = 'Sprites/blackPawn.png'
+        Position = {
+            'x': 165,
+            'y': 295,
         }
+
     class blackPawn2:
-        paths='Sprites/blackPawn.png'
-        Position={
-            'x':303,
-            'y':295,
+        paths = 'Sprites/blackPawn.png'
+        Position = {
+            'x': 303,
+            'y': 295,
         }
+
     class blackPawn3:
-        paths="Sprites/blackPawn.png"
-        Position={
-            'x':455,
-            'y':295,
+        paths = "Sprites/blackPawn.png"
+        Position = {
+            'x': 455,
+            'y': 295,
         }
+
     class blackPawn4:
-        paths='Sprites/blackPawn.png'
-        Position={
-            'x':559,
-            'y':295,
+        paths = 'Sprites/blackPawn.png'
+        Position = {
+            'x': 559,
+            'y': 295,
         }
+
     class blackPawn5:
-        paths="Sprites/blackPawn.png"
-        Position={
-            'x':705,
-            'y':295,
+        paths = "Sprites/blackPawn.png"
+        Position = {
+            'x': 705,
+            'y': 295,
         }
+
     class blackPawn6:
-        paths="Sprites/blackPawn.png"
-        Position={
-            'x':836,
-            'y':295,
+        paths = "Sprites/blackPawn.png"
+        Position = {
+            'x': 836,
+            'y': 295,
         }
+
     class blackPawn7:
-        paths="Sprites/blackPawn.png"
-        Position={
-            'x':954,
-            'y':295,
+        paths = "Sprites/blackPawn.png"
+        Position = {
+            'x': 954,
+            'y': 295,
         }
+
     class blackPawn8:
-        paths="Sprites/blackPawn.png"
-        Position={
-            'x':1103,
-            'y':295,
+        paths = "Sprites/blackPawn.png"
+        Position = {
+            'x': 1103,
+            'y': 295,
         }
+
     class blackRook1:
-        paths="Sprites/blackRook.png"
-        Position={
-            'x':185,
-            'y':180,
+        paths = "Sprites/blackRook.png"
+        Position = {
+            'x': 185,
+            'y': 180,
         }
+
     class blackRook2:
-        paths="Sprites/blackRook.png"
-        Position={
-            'x':1103,
-            'y':180,
+        paths = "Sprites/blackRook.png"
+        Position = {
+            'x': 1103,
+            'y': 180,
         }
+
     class blackKnight1:
-        paths="Sprites/blackKnight.png"
-        Position={
-            'x':966,
-            'y':180,
+        paths = "Sprites/blackKnight.png"
+        Position = {
+            'x': 966,
+            'y': 180,
         }
+
     class blackKnight2:
-        paths="Sprites/blackKnight.png"
-        Position={
-            'x':327,
-            'y':180,
+        paths = "Sprites/blackKnight.png"
+        Position = {
+            'x': 327,
+            'y': 180,
         }
+
     class blackQueen:
-        paths="Sprites/blackQueen.png"
-        Position={
-            'x':585,
-            'y':180.
+        paths = "Sprites/blackQueen.png"
+        Position = {
+            'x': 585,
+            'y': 180.
         }
+
     class blackKing:
-        paths="Sprites/blackKing.png"
-        Position={
-            'x':704,
-            'y':180,
+        paths = "Sprites/blackKing.png"
+        Position = {
+            'x': 704,
+            'y': 180,
         }
