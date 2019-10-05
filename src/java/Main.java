072a14d8e569c95df31e687e00f78e5d87cefd15
thecla1 <thecla1@nilesk12.org>
2019-10-05 21:07:26 +0000

    Changes

--- src/java/Main.java
+++ src/java/Main.java
@@ -2,7 +2,8 @@ package java;
 
 class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World From Java!");
+        System.out.println("Hello World From Java!");
+
     }
 
 }
