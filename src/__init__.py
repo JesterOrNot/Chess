faf3f69765b4872a3528c41fce75321215cc52e1
Sean Hellum <seanhellum45@gmail.com>
2019-10-07 00:16:41 +0000

    more change

---
+++ src/__init__.py
@@ -0,0 +1,225 @@
+# Declares the piece's classes with the path to it's sprite and the default x and y coordinate to plot them at
+class Pieces:
+    class WhitePawn1:
+        paths = "Sprites/whitePawn.png"
+        Position = {
+            'x': 170,
+            'y': 960,
+        }
+
+    class WhitePawn2:
+        paths = "Sprites/whitePawn.png"
+        Position = {
+            'x': 320,
+            'y': 960,
+        }
+
+    class WhitePawn3:
+        paths = "Sprites/whitePawn.png"
+        Position = {
+            'x': 440,
+            'y': 960,
+        }
+
+    class WhitePawn4:
+        paths = "Sprites/whitePawn.png"
+        Position = {
+            'x': 590,
+            'y': 960,
+        }
+
+    class WhitePawn5:
+        paths = "Sprites/whitePawn.png"
+        Position = {
+            'x': 710,
+            'y': 960,
+        }
+
+    class WhitePawn6:
+        paths = "Sprites/whitePawn.png"
+        Position = {
+            'x': 830,
+            'y': 960,
+        }
+
+    class WhitePawn7:
+        paths = "Sprites/whitePawn.png"
+        Position = {
+            'x': 980,
+            'y': 960,
+        }
+
+    class WhitePawn8:
+        paths = "Sprites/whitePawn.png"
+        Position = {
+            'x': 1100,
+            'y': 960,
+        }
+
+    class WhiteBishop1:
+        paths = "Sprites/whiteBishop.png"
+        Position = {
+            'x': 440,
+            'y': 1100,
+        }
+
+    class WhiteBishop2:
+        paths = "Sprites/whiteBishop.png"
+        Position = {
+            'x': 830,
+            'y': 1100,
+        }
+
+    class BlackBishop1:
+        paths = 'Sprites/blackBishop.png'
+        Position = {
+            'x': 440,
+            'y': 180,
+        }
+
+    class BlackBishop2:
+        paths = "Sprites/blackBishop.png"
+        Position = {
+            'x': 830,
+            'y': 180,
+        }
+
+    class WhiteRook1:
+        paths = 'Sprites/whiteRook.png'
+        Position = {
+            'x': 185,
+            'y': 1088,
+        }
+
+    class WhiteRook2:
+        paths = "Sprites/whiteRook.png"
+        Position = {
+            'x': 1103,
+            'y': 1088,
+        }
+
+    class WhiteKnight1:
+        paths = 'Sprites/whiteKnight.png'
+        Position = {
+            'x': 966,
+            'y': 1088,
+        }
+
+    class WhiteKnight2:
+        paths = "Sprites/whiteKnight.png"
+        Position = {
+            'x': 327,
+            'y': 1088,
+        }
+
+    class WhiteQueen:
+        paths = "Sprites/whiteQueen.png"
+        Position = {
+            'x': 585,
+            'y': 1100,
+        }
+
+    class WhiteKing:
+        paths = "Sprites/whiteKing.png"
+        Position = {
+            'x': 704,
+            'y': 1100,
+        }
+
+    class BlackPawn1:
+        paths = 'Sprites/blackPawn.png'
+        Position = {
+            'x': 165,
+            'y': 295,
+        }
+
+    class BlackPawn2:
+        paths = 'Sprites/blackPawn.png'
+        Position = {
+            'x': 303,
+            'y': 295,
+        }
+
+    class BlackPawn3:
+        paths = "Sprites/blackPawn.png"
+        Position = {
+            'x': 455,
+            'y': 295,
+        }
+
+    class BlackPawn4:
+        paths = 'Sprites/blackPawn.png'
+        Position = {
+            'x': 559,
+            'y': 295,
+        }
+
+    class BlackPawn5:
+        paths = "Sprites/blackPawn.png"
+        Position = {
+            'x': 705,
+            'y': 295,
+        }
+
+    class BlackPawn6:
+        paths = "Sprites/blackPawn.png"
+        Position = {
+            'x': 836,
+            'y': 295,
+        }
+
+    class BlackPawn7:
+        paths = "Sprites/blackPawn.png"
+        Position = {
+            'x': 954,
+            'y': 295,
+        }
+
+    class BlackPawn8:
+        paths = "Sprites/blackPawn.png"
+        Position = {
+            'x': 1103,
+            'y': 295,
+        }
+
+    class BlackRook1:
+        paths = "Sprites/blackRook.png"
+        Position = {
+            'x': 185,
+            'y': 180,
+        }
+
+    class BlackRook2:
+        paths = "Sprites/blackRook.png"
+        Position = {
+            'x': 1103,
+            'y': 180,
+        }
+
+    class BlackKnight1:
+        paths = "Sprites/blackKnight.png"
+        Position = {
+            'x': 966,
+            'y': 180,
+        }
+
+    class BlackKnight2:
+        paths = "Sprites/blackKnight.png"
+        Position = {
+            'x': 327,
+            'y': 180,
+        }
+
+    class BlackQueen:
+        paths = "Sprites/blackQueen.png"
+        Position = {
+            'x': 585,
+            'y': 180.
+        }
+
+    class BlackKing:
+        paths = "Sprites/blackKing.png"
+        Position = {
+            'x': 704,
+            'y': 180,
+        }
--- src/chessBoard.py
+++ src/chessBoard.py
@@ -3,81 +3,81 @@ import matplotlib.image as mpimg
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
 import __init__ # imports all the piece classes
-pieces = __init__.pieces() # declares the class pieces locally as 'pieces'
+pieces = __init__.Pieces() # declares the class pieces locally as 'pieces'
 
 # This function plots the image data for the sprites
 def board():
     def getImage(path):
         return OffsetImage(plt.imread(path))
 # This gets sprites images and puts it in a list
-    paths = [pieces.whitePawn1.paths, pieces.whitePawn2.paths, pieces.whitePawn3.paths, pieces.whitePawn4.paths, pieces.whitePawn5.paths, pieces.whitePawn6.paths,
-             pieces.whitePawn7.paths, pieces.whitePawn1.paths, pieces.whiteBishop1.paths, pieces.whiteBishop2.paths, pieces.blackBishop1.paths, pieces.blackBishop2.paths,
-             pieces.whiteRook1.paths,
-             pieces.whiteRook2.paths,
-             pieces.whiteKnight1.paths,
-             pieces.whiteKnight2.paths,
-             pieces.whiteQueen.paths,
-             pieces.whiteKing.paths,
-             pieces.blackPawn1.paths,
-             pieces.blackPawn2.paths,
-             pieces.blackPawn3.paths,
-             pieces.blackPawn4.paths,
-             pieces.blackPawn5.paths,
-             pieces.blackPawn6.paths,
-             pieces.blackPawn7.paths,
-             pieces.blackPawn8.paths,
-             pieces.blackRook1.paths,
-             pieces.blackRook2.paths,
-             pieces.blackKnight1.paths,
-             pieces.blackKnight2.paths,
-             pieces.blackQueen.paths,
-             pieces.blackKing.paths]
+    paths = [pieces.WhitePawn1.paths, pieces.WhitePawn2.paths, pieces.WhitePawn3.paths, pieces.WhitePawn4.paths, pieces.WhitePawn5.paths, pieces.WhitePawn6.paths,
+             pieces.WhitePawn7.paths, pieces.WhitePawn1.paths, pieces.WhiteBishop1.paths, pieces.WhiteBishop2.paths, pieces.BlackBishop1.paths, pieces.BlackBishop2.paths,
+             pieces.WhiteRook1.paths,
+             pieces.WhiteRook2.paths,
+             pieces.WhiteKnight1.paths,
+             pieces.WhiteKnight2.paths,
+             pieces.WhiteQueen.paths,
+             pieces.WhiteKing.paths,
+             pieces.BlackPawn1.paths,
+             pieces.BlackPawn2.paths,
+             pieces.BlackPawn3.paths,
+             pieces.BlackPawn4.paths,
+             pieces.BlackPawn5.paths,
+             pieces.BlackPawn6.paths,
+             pieces.BlackPawn7.paths,
+             pieces.BlackPawn8.paths,
+             pieces.BlackRook1.paths,
+             pieces.BlackRook2.paths,
+             pieces.BlackKnight1.paths,
+             pieces.BlackKnight2.paths,
+             pieces.BlackQueen.paths,
+             pieces.BlackKing.paths]
 # This get's each sprites x position
-    x = [pieces.whitePawn1.Position.get("x"), pieces.whitePawn2.Position.get("x"), pieces.whitePawn3.Position.get("x"), pieces.whitePawn4.Position.get("x"), pieces.whitePawn5.Position.get("x"), pieces.whitePawn6.Position.get(
-        "x"), pieces.whitePawn7.Position.get("x"), pieces.whitePawn8.Position.get("x"), pieces.whiteBishop1.Position.get("x"), pieces.whiteBishop2.Position.get("x"), pieces.blackBishop1.Position.get("x"), pieces.blackBishop2.Position.get("x"),
-        pieces.whiteRook1.Position.get("x"),
-        pieces.whiteRook2.Position.get("x"),
-        pieces.whiteKnight1.Position.get("x"),
-        pieces.whiteKnight2.Position.get("x"),
-        pieces.whiteQueen.Position.get("x"),
-        pieces.whiteKing.Position.get("x"),
-        pieces.blackPawn1.Position.get("x"),
-        pieces.blackPawn2.Position.get("x"),
-        pieces.blackPawn3.Position.get("x"),
-        pieces.blackPawn4.Position.get("x"),
-        pieces.blackPawn5.Position.get("x"),
-        pieces.blackPawn6.Position.get("x"),
-        pieces.blackPawn7.Position.get("x"),
-        pieces.blackPawn8.Position.get("x"),
-        pieces.blackRook1.Position.get("x"),
-        pieces.blackRook2.Position.get("x"),
-        pieces.blackKnight1.Position.get("x"),
-        pieces.blackKnight2.Position.get("x"),
-        pieces.blackQueen.Position.get("x"),
-        pieces.blackKing.Position.get("x")]
+    x = [pieces.WhitePawn1.Position.get("x"), pieces.WhitePawn2.Position.get("x"), pieces.WhitePawn3.Position.get("x"), pieces.WhitePawn4.Position.get("x"), pieces.WhitePawn5.Position.get("x"), pieces.WhitePawn6.Position.get(
+        "x"), pieces.WhitePawn7.Position.get("x"), pieces.WhitePawn8.Position.get("x"), pieces.WhiteBishop1.Position.get("x"), pieces.WhiteBishop2.Position.get("x"), pieces.BlackBishop1.Position.get("x"), pieces.BlackBishop2.Position.get("x"),
+        pieces.WhiteRook1.Position.get("x"),
+        pieces.WhiteRook2.Position.get("x"),
+        pieces.WhiteKnight1.Position.get("x"),
+        pieces.WhiteKnight2.Position.get("x"),
+        pieces.WhiteQueen.Position.get("x"),
+        pieces.WhiteKing.Position.get("x"),
+        pieces.BlackPawn1.Position.get("x"),
+        pieces.BlackPawn2.Position.get("x"),
+        pieces.BlackPawn3.Position.get("x"),
+        pieces.BlackPawn4.Position.get("x"),
+        pieces.BlackPawn5.Position.get("x"),
+        pieces.BlackPawn6.Position.get("x"),
+        pieces.BlackPawn7.Position.get("x"),
+        pieces.BlackPawn8.Position.get("x"),
+        pieces.BlackRook1.Position.get("x"),
+        pieces.BlackRook2.Position.get("x"),
+        pieces.BlackKnight1.Position.get("x"),
+        pieces.BlackKnight2.Position.get("x"),
+        pieces.BlackQueen.Position.get("x"),
+        pieces.BlackKing.Position.get("x")]
 # This gets the pieces y position
-    y = [pieces.whitePawn1.Position.get("y"), pieces.whitePawn2.Position.get(
-        "y"), pieces.whitePawn3.Position.get("y"), pieces.whitePawn4.Position.get("y"), pieces.whitePawn5.Position.get("y"), pieces.whitePawn6.Position.get("y"), pieces.whitePawn7.Position.get("y"), pieces.whitePawn8.Position.get("y"), pieces.whiteBishop1.Position.get("y"), pieces.whiteBishop2.Position.get("y"), pieces.blackBishop1.Position.get("y"), pieces.blackBishop2.Position.get("y"),
-        pieces.whiteRook1.Position.get("y"),
-        pieces.whiteRook2.Position.get("y"),
-        pieces.whiteKnight1.Position.get("y"),
-        pieces.whiteKnight2.Position.get("y"),
-        pieces.whiteQueen.Position.get('y'),
-        pieces.whiteKing.Position.get("y"),
-        pieces.blackPawn1.Position.get("y"),
-        pieces.blackPawn2.Position.get("y"),
-        pieces.blackPawn3.Position.get("y"),
-        pieces.blackPawn4.Position.get("y"),
-        pieces.blackPawn5.Position.get("y"),
-        pieces.blackPawn6.Position.get("y"),
-        pieces.blackPawn7.Position.get("y"),
-        pieces.blackPawn8.Position.get("y"),
-        pieces.blackRook1.Position.get("y"),
-        pieces.blackRook2.Position.get("y"),
-        pieces.blackKnight1.Position.get("y"),
-        pieces.blackKnight2.Position.get("y"),
-        pieces.blackQueen.Position.get("y"),
-        pieces.blackKing.Position.get("y")]
+    y = [pieces.WhitePawn1.Position.get("y"), pieces.WhitePawn2.Position.get(
+        "y"), pieces.WhitePawn3.Position.get("y"), pieces.WhitePawn4.Position.get("y"), pieces.WhitePawn5.Position.get("y"), pieces.WhitePawn6.Position.get("y"), pieces.WhitePawn7.Position.get("y"), pieces.WhitePawn8.Position.get("y"), pieces.WhiteBishop1.Position.get("y"), pieces.WhiteBishop2.Position.get("y"), pieces.BlackBishop1.Position.get("y"), pieces.BlackBishop2.Position.get("y"),
+        pieces.WhiteRook1.Position.get("y"),
+        pieces.WhiteRook2.Position.get("y"),
+        pieces.WhiteKnight1.Position.get("y"),
+        pieces.WhiteKnight2.Position.get("y"),
+        pieces.WhiteQueen.Position.get('y'),
+        pieces.WhiteKing.Position.get("y"),
+        pieces.BlackPawn1.Position.get("y"),
+        pieces.BlackPawn2.Position.get("y"),
+        pieces.BlackPawn3.Position.get("y"),
+        pieces.BlackPawn4.Position.get("y"),
+        pieces.BlackPawn5.Position.get("y"),
+        pieces.BlackPawn6.Position.get("y"),
+        pieces.BlackPawn7.Position.get("y"),
+        pieces.BlackPawn8.Position.get("y"),
+        pieces.BlackRook1.Position.get("y"),
+        pieces.BlackRook2.Position.get("y"),
+        pieces.BlackKnight1.Position.get("y"),
+        pieces.BlackKnight2.Position.get("y"),
+        pieces.BlackQueen.Position.get("y"),
+        pieces.BlackKing.Position.get("y")]
     fig, ax = plt.subplots() # declares that the graph will be shown as a subplot (for asthetics and to declare a variable for the subplot)
     # This declares that the plot is a scatter_plot
     ax.scatter(x, y)
@@ -87,12 +87,12 @@ def board():
     # plt.xticks([])  ## Uncomment this when in deployment
     # plt.yticks([])  ## Uncomment this when in deployment
     plt.grid(which='both',axis='both') # Comment this in deployment it shows the grid on the plot
-    img = mpimg.imread('chessBoard.png') ## Creates the background image in this case the chess board
+    img = mpimg.imread('Sprites/chessBoard.png') ## Creates the background image in this case the chess board
     imgplot = plt.imshow(img) ## shows the board
     for x0, y0, path in zip(x, y, paths): ## Loops through the lists and uses zip to make a set for each peice containing the path to the image and it's respective x & y coordinate
         ab = AnnotationBbox(getImage(path), (x0, y0), frameon=False) ## reads the sprites
         ax.add_artist(ab) ### Shows the sprites
-    plt.show() ### Displays the subplot
+    plt.savefig("theBoard/chessBoard.png") ### Displays the subplot
 
 
 board()
